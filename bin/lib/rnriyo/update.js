6cbd1b1ef06d9ee8d0dc3a7eeb59315068e4e9b6
Rejjak Ali <rejjakali94@gtmail.com>
2019-06-10 23:36:28 +0530

    Minor bug cleaned up

--- bin/lib/rnriyo/update.js
+++ bin/lib/rnriyo/update.js
@@ -1,14 +1,10 @@
-const https = require('https');
 const { exec } = require('child_process');
 module.exports = function(callback){
-  exec('npm view rn-riyo', (err, stdout, stderr) => {
+  exec('npm view rn-riyo version', (err, stdout, stderr) => {
     if (err) {
       callback('error');
       return;
     }
-    let start_pos = stdout.indexOf("latest: '")+9;
-    let end_pos = stdout.indexOf("' }");
-    let latest_version = stdout.substr(start_pos,end_pos-start_pos);
-    callback(latest_version);
+    callback(stdout);
   });
 }
--- cli.js
+++ cli.js
@@ -10,35 +10,58 @@ const month = date.getMonth()+1;
 const year = date.getFullYear();
 const final_date = day+'/'+month+'/'+year;
 const package = require('./bin/lib/rnriyo/package');
+//const compareVersions = require('compare-versions');
+function compareVersions(v1, comparator, v2) {
+    "use strict";
+    var comparator = comparator == '=' ? '==' : comparator;
+    if(['==','===','<','<=','>','>=','!=','!=='].indexOf(comparator) == -1) {
+        throw new Error('Invalid comparator. ' + comparator);
+    }
+    var v1parts = v1.split('.'), v2parts = v2.split('.');
+    var maxLen = Math.max(v1parts.length, v2parts.length);
+    var part1, part2;
+    var cmp = 0;
+    for(var i = 0; i < maxLen && !cmp; i++) {
+        part1 = parseInt(v1parts[i], 10) || 0;
+        part2 = parseInt(v2parts[i], 10) || 0;
+        if(part1 < part2)
+            cmp = 1;
+        if(part1 > part2)
+            cmp = -1;
+    }
+    return eval('0' + comparator + cmp);
+}
 cli.init = ()=> {
-	if(fs.existsSync('node_modules/rncli/file.txt')){
-		fs.readFile('node_modules/rncli/file.txt',"utf-8",(err,data)=>{
-			if(final_date == data){
-				riyoNative.riyoVersion(program,process);
-		    	riyoNative.riyoHelp(program,process);
-		    	riyoNative.riyoCommander(program,process);
-			}else{
-				update(function(newVersion){
-					if(newVersion != 'error'){
-						package(function(oldVersion){
-							if(newVersion != oldVersion.version){
-								riyoNative.askForUpdate(function(result){
+	if(fs.existsSync('node_modules')){
+		if(fs.existsSync('node_modules/rncli/file.txt')){
+			fs.readFile('node_modules/rncli/file.txt',"utf-8",(err,data)=>{
+				if(final_date.toString() == data.toString()){
+					riyoInit();
+				}else{
+					update(function(newVersion){
+						if(newVersion != 'error'){
+							package(function(oldVersion){
+								if(compareVersions(newVersion,'==',oldVersion.version)){
 									constDateForADay();
-								});
-							}else{
-								constDateForADay(); 
-							}
-						});
-					}else{
-						constDateForADay();
-					}
-				});
-			}
-		})
+								}else{
+									riyoNative.askForUpdate(function(result){
+										constDateForADay();
+									});
+								}
+							});
+						}else{
+							constDateForADay();
+						}
+					});
+				}
+			})
+		}else{
+			riyoNative.firstInstall(function(){
+				riyoInit();
+			});                     
+		}
 	}else{
-		riyoNative.firstInstall(function(){
-			riyoInit();
-		});                     
+		riyoInit();
 	}
 }
 
